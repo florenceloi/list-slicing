92345574150aed78edf03b90cbe211fb9368d0ba
Florence Loi <florence.c.loi@gmail.com>
2016-01-07 16:22:31 -0800

    Fixed all functions; 14 tests passed

--- list_operations.py
+++ list_operations.py
@@ -95,7 +95,7 @@ def first_three(input_list):
 
     """
 
-    return []
+    return input_list[0:3]
 
 
 def last_five(input_list):
@@ -109,7 +109,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
@@ -123,7 +123,7 @@ def middle(input_list):
 
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
@@ -137,7 +137,7 @@ def inner_four(input_list):
 
     """
 
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
@@ -152,7 +152,7 @@ def inner_four_end(input_list):
 
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
@@ -169,6 +169,7 @@ def replace_head(input_list):
 
     """
 
+    input_list[0] = 42
     pass
 
 
@@ -186,6 +187,8 @@ def replace_third_and_last(input_list):
 
     """
 
+    input_list[2] = 37
+    input_list[-1] = 37
     pass
 
 
@@ -203,6 +206,7 @@ def replace_middle(input_list):
 
     """
 
+    input_list[2:-2] = [42, 37]
     pass
 
 
@@ -220,6 +224,8 @@ def delete_third_and_seventh(input_list):
 
     """
 
+    del input_list[6]
+    del input_list[2]
     pass
 
 
@@ -237,6 +243,7 @@ def delete_middle(input_list):
 
     """
 
+    input_list[2:-2] = []
     pass
 
 
